a2a1f80dd808ae753128c21ee61794e4126f5174
shapilova <p_i.a.shapilova@.mpt.ru>
2019-11-18 14:01:41 +0300

    Ready session 1

---
+++ Ka/Ka/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ka
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection connection = new SqlConnection("Data Source=172.20.10.8:1433;Initial Catalog=Speed;Integrated Security=True");
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            connection.Open();
+            SqlCommand command = new SqlCommand("insert into [dbo].[User] (name) values ('" + textBox1.Text + "')", connection);
+            command.ExecuteNonQuery();
+            connection.Close();
+
+        }
+    }
+}
